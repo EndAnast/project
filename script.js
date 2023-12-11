31808f935954f11b2f48bc9cd26deba66b3f2601
Nastya <example@gmail.com>
2023-12-11 10:08:28 +0300

    Added h1 tag  with '#' will be ignored, and an empty message aborts the commit.

--- script.js
+++ script.js
@@ -3,5 +3,5 @@
 // const result = confirm ('Are you here?');
 // console.log (result);
 
-const answer = prompt ('Тебе есть 18?', '');
-console.log(typeof(answer));
+// const answer = prompt ('Тебе есть 18?', '');
+// console.log(typeof(answer));
