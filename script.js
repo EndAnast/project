aaa3c1250a22e27ccec29631321fb33cd580d7a1
Nastya <example@gmail.com>
2023-12-13 17:17:48 +0300

    add answers

--- script.js
+++ script.js
@@ -5,4 +5,17 @@
 
 // const answer = prompt ('Тебе есть 18?', '');
 // console.log(typeof(answer));
-const numberOfFilms = prompt ("Сколько фильмов вы уже смотерли?")
+const numberOfFilms = prompt ('Сколько фильмов вы уже смотерли?', '');
+console.log (numberOfFilms);
+
+const personalMovieDfa = {
+count: numberOfFilms,
+movies: {},
+actors: {},
+genres: [],
+privat: false
+};
+console.log (personalMovieDfa);
+
+const answer = [];
+answer [0] = prompt ('Один из последних просмотренных фильмов')
